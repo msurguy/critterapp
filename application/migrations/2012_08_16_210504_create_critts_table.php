b89b16033a5efdfd7fc447635a4c5186e64a812b
Maks Surguy <msurguy@gmail.com>
2012-08-21 23:44:32 -0700

    updated DB

--- application/migrations/2012_08_16_210504_create_critts_table.php
+++ application/migrations/2012_08_16_210504_create_critts_table.php
@@ -9,7 +9,7 @@ class Create_Critts_Table {
 			$table->integer('user_id');
 			$table->text('critt');
 			$table->timestamps();
-			$table->foreign('user_id')->references('id')->on('users')-> on_delete('cascade');
+			//$table->foreign('user_id')->references('id')->on('users')->on_delete('cascade');
 		});
 	}
 
